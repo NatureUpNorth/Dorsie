f6f8001163d36a981546e526bbdcb35f0a6a6c4c
Cooper Olney <colney2023@gmail.com>
2026-01-25 12:59:25 -0500

    organize files and update readme

--- uploader/app.py
+++ uploader/app.py
@@ -3,15 +3,18 @@ import json
 from flask import Flask, render_template, request, flash, session, jsonify, redirect, url_for
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 
+# App Setup
 app = Flask(__name__)
 app.secret_key = "secret"
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
+# Login Manager Setup
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
 
+# User Setup and Login (Prototype)
 class User(UserMixin):
     def __init__(self, id):
         self.id = id
@@ -30,6 +33,7 @@ def load_user(user_id):
     return users.get(user_id)
 
 
+# Login Route
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -55,23 +59,28 @@ def login():
     """
 
 
+# Allowed File Extensions
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'avi', 'mov'}
-
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+# Page Routes
+
+# Main page route
 @app.route("/index")
 @login_required
 def index():
     return render_template("index.html")
 
+# Location/Map route (not sure if still needed)
 @app.route("/location")
 @login_required
 def location():
     return render_template("map.html")
 
+
+# Other "routes" (not actual pages)
 
 @app.route("/save_dates", methods=["POST"])
 @login_required
